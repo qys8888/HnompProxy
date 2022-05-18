999fefe68a5b5a005b453a75b82b541d1581e0a7
HnompProxy <66261165+qys8888@users.noreply.github.com>
2022-05-18 23:20:51 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+[[ $(id -u) != 0 ]] && echo -e "请使用root权限运行安装脚本" && exit 1
+
+cmd="apt-get"
+if [[ $(command -v apt-get) || $(command -v yum) ]] && [[ $(command -v systemctl) ]]; then
+    if [[ $(command -v yum) ]]; then
+        cmd="yum"
+    fi
+else
+    echo "此脚本不支持该系统" && exit 1
+fi
+
+install() {
+    if [ -f "/usr/bin/HnompProxy" ]; then
+        echo -e "您已安装了该软件，如果确定没有安装，请使用此脚本的卸载功能后重新安装" && exit 1
+    fi
+    if pgrep HnompProxy; then
+        echo -e "检测到您已启动了 /usr/bin/HnompProxy，请关闭后再安装！" && exit 1
+    fi
+
+    $cmd update -y
+    $cmd install curl wget -y
+    mkdir /etc/HnompProxy
+
+    echo "请选择版本"
+    echo "  1、v0.0.1 | 代号 [Rinako]"
+    read -p "$(echo -e "请输入[1-4]：")" choose
+    case $choose in
+    1)
+        wget https://github.com/ethpoolproxy/HnompProxy/releases/download/v1.0.1/HnompProxy -O /usr/bin/HnompProxy
+        ;;
+        echo "请输入正确的数字"
+        ;;
+    esac
+    
+    wget https://raw.githubusercontent.com/qys8888/HnompProxy/HnompProxy.service -O /etc/systemd/system/HnompProxy.service
+    chmod +x /usr/bin/HnompProxy
+
+    echo "正在启动..."
+    systemctl disable --now firewalld
+    systemctl daemon-reload
+    systemctl enable --now HnompProxy
+    sleep 2s
+    journalctl --unit=HnompProxy --no-tail  --no-full --no-pager --no-hostname --lines=10
+    echo "安装结束!"
+    echo "后台管理地址(请以实际外网IP为准): http://$(curl --silent ifconfig.me):8444"
+}
+
+uninstall() {
+    read -p "是否确认删除 HnompProxy [yes/no]：" flag
+    if [ -z $flag ]; then
+        echo "输入错误" && exit 1
+    else
+        if [ "$flag" = "yes" -o "$flag" = "ye" -o "$flag" = "y" ]; then
+            systemctl disable --now HnompProxy
+            rm -rf /etc/systemd/system/HnompProxy.service
+            rm -rf /usr/bin/HnompProxy
+            rm -rf /etc/HnompProxy
+            systemctl daemon-reload
+            echo "卸载 HnompProxy 成功"
+        fi
+    fi
+}
+
+start() {
+    systemctl enable --now HnompProxy
+    sleep 2s
+    journalctl --unit=HnompProxy --no-tail  --no-full --no-pager --no-hostname --lines=10
+
+    echo "HnompProxy 已启动"
+}
+
+restart() {
+    systemctl restart HnompProxy
+    sleep 2s
+    journalctl --unit=HnompProxy --no-tail  --no-full --no-pager --no-hostname --lines=10
+
+    echo "HnompProxy 重新启动成功"
+}
+
+stop() {
+    systemctl stop HnompProxy
+    echo "HnompProxy 已停止"
+}
+
+show_log(){
+    echo -n "最近的 100 行日志: "
+    journalctl --unit=HnompProxy --no-tail  --no-full --no-pager --no-hostname --lines=100
+}
+
+check_limit(){
+    echo -n "当前连接数限制：102400"
+}
+
+uninstall_tx_mon() {
+    /usr/local/qcloud/YunJing/uninst.sh
+    /usr/local/qcloud/stargate/admin/uninstall.sh
+    /usr/local/qcloud/monitor/barad/admin/uninstall.sh
+    systemctl stop tat_agent
+    systemctl disable tat_agent
+    rm -rf /etc/systemd/system/tat_agent.service
+    rm -rf /etc/systemd/system/cloud-init.target.wants
+    rm -rf /usr/local/qcloud/
+    rm -rf /usr/local/yd.socket.server
+    echo -n "腾讯云监控卸载成功！"
+}
+
+echo "============================ HnompProxy ============================"
+echo "  1、安装(安装到 程序:/usr/bin/HnompProxy 配置文件:/etc/HnompProxy)"
+echo "  2、卸载(更新请先卸载，请注意: 配置文件不兼容 需要重新配置)"
+echo "  3、启动"
+echo "  4、重启"
+echo "  5、停止"
+echo "  6、查看最近的 100 行日志"
+echo "  7、查看软件连接数限制"
+echo "  8、卸载腾讯云监控"
+echo "======================================================================"
+read -p "$(echo -e "请选择[1-6]：")" choose
+case $choose in
+1)
+    install
+    ;;
+2)
+    uninstall
+    ;;
+3)
+    start
+    ;;
+4)
+    restart
+    ;;
+5)
+    stop
+    ;;
+6)
+    show_log
+    ;;
+7)
+    check_limit
+    ;;
+8)
+    uninstall_tx_mon
+    ;;
+*)
+    echo "输入错误，请重新输入！"
+    ;;
+esac
